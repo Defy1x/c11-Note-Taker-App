cbf4b409343d8be2d2e699f3663a821b94ba5382
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-04-18 20:18:04 -0400

    getting notes need to add new ones and delete

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Dependencies
-
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-// const noteData = require('./db/db.json');
+const { v4: uuidv4 } = require("uuid");
+const data = require("./db/db.json");
 
 // Sets up the Express App
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -13,62 +13,31 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//get the css
 app.use(express.static(__dirname + '/public'));
 
-// Star Wars Characters (DATA)
-
-const notesData = [
-  {
-    title: 'yoda',
-    text: 'Yoda',
-    id: 'Jedi Master',
-  },
-];
-
-// Routes
-
-// res.send("Welcome to the Star Wars Page!")
+// HTML Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-// // Displays all characters
-// app.get('/notes', (req, res) => res.json(notesData));
-
-// Displays a single character, or returns false
-app.get('/api/characters/:character', (req, res) => {
-  const chosen = req.params.character;
-
-  console.log(chosen);
-
-  /* Check each character routeName and see if the same as "chosen"
-   If the statement is true, send the character back as JSON,
-   otherwise send the boolean value false as JSON */
-
-  for (let i = 0; i < characters.length; i++) {
-    if (chosen === characters[i].routeName) {
-      return res.json(characters[i]);
-    }
-  }
-
-  return res.json(false);
-});
+//API Routes, Displays all notes
+app.get('/api/notes', (req, res) => res.json(data));
 
 // Create New Characters - takes in JSON input
-app.post('/api/characters', (req, res) => {
+app.post('/api/notes', (req, res) => {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body parsing middleware
-  const newcharacter = req.body;
+  const newnote = req.body;
 
-  console.log(newcharacter);
+  console.log(newnote);
 
   // We then add the json the user sent to the character array
-  characters.push(newcharacter);
+  data.push(newcharacter);
 
   // We then display the JSON to the users
-  res.json(newcharacter);
+  res.json(data);
 });
 
 // Starts the server to begin listening
-
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
